046c934f4328fe738ebd998ee1e2ffe04f858229
Pressy674 <thomas.leleu67@gmail.com>
2025-04-01 10:20:32 +0200

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import TiltNation from './TiltNation'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <TiltNation />
-  </React.StrictMode>,
-)
+const App = () => {
+  return (
+    <div className="p-6 text-center">
+      <img src="/logo.png" alt="Tilt Nation Logo" className="mx-auto w-32 h-32" />
+      <h1 className="text-2xl font-bold mt-4">Tilt Nation</h1>
+      <p className="text-gray-600 mt-2">Bienvenue sur la plateforme de suivi de vos soirées poker.</p>
+    </div>
+  )
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
